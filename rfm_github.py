462994e1da690fe00b9a57aa14c2d7c7700001a0
Yağmur Sak <81710234+YagmurSak@users.noreply.github.com>
2024-09-15 23:05:08 +0300

    RFM Segmentation.py

--- rfm_github.py
+++ rfm_github.py
@@ -4,33 +4,6 @@ from numba.core.runtime import rtsys
 
 pd.set_option('display.max_columns', None)
 
-
-#### Customer Segmentation with RFM #####
-
-#### Business Problem:
-
-# FLO wants to segment its customers and determine marketing strategies according to these segments.
-# For this purpose, customer behaviors will be defined and groups will be created according to these behavior clusters.
-
-# The dataset consists of information obtained from the past shopping behaviors of customers who made their last
-# purchases as OmniChannel (both online and offline shopping) in 2020 - 2021.
-
-# VARIABLES
-#master_id: Unique customer number
-#order_channel: Which channel is used for the shopping platform (Android, ios, Desktop, Mobile, Offline)
-#last_order_channel: The channel where the last shopping was made
-#first_order_date: The customer's first shopping date
-#last_order_date: The customer's last shopping date
-#last_order_date_online: The customer's last shopping date on the online platform
-#last_order_date_offline: The customer's last shopping date on the offline platform
-#order_num_total_ever_online: The total number of shopping made by the customer on the online platform
-#order_num_total_ever_offline: The total number of shopping made by the customer offline
-#customer_value_total_ever_offline: The total amount paid by the customer for offline shopping
-#customer_value_total_ever_online: The total amount paid by the customer for online shopping
-#customer_value_total_ever_online: The total amount paid by the customer for online shopping
-#interested_in_categories_12: The customer's last List of categories shopped in the last 12 months
-
-
 ### Data Preparation and First Insight
 
 df_= pd.read_csv(r"C:\Users\Yagmu\PycharmProjects\pythonProject2\flo_data_20k.csv")
