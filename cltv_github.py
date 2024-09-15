ac2d8acc2e84edaeb887cf09b6188eb27e01562e
Yağmur Sak <81710234+YagmurSak@users.noreply.github.com>
2024-09-15 23:04:31 +0300

    cltv_prediction.py

--- cltv_github.py
+++ cltv_github.py
@@ -1,29 +1,3 @@
-### CLTV Prediction with BG-NBD and Gamma-Gamma ###
-
-## Business Problem:
-    #FLO Compony wants to determine a roadmap for sales and marketing activities.
-# In order for the company to make a medium-long term plan, the potential value that existing
-# customers will provide to the company in the future must be estimated. The dataset consists of
-# information obtained from the past shopping behavior of customers who made their last purchases
-# as OmniChannel (both online and offline shopping) in 2020 - 2021.
-
-
-# VARIABLES
-#master_id: Unique customer number
-#order_channel: Which channel is used for the shopping platform (Android, ios, Desktop, Mobile, Offline)
-#last_order_channel: The channel where the last shopping was made
-#first_order_date: The customer's first shopping date
-#last_order_date: The customer's last shopping date
-#last_order_date_online: The customer's last shopping date on the online platform
-#last_order_date_offline: The customer's last shopping date on the offline platform
-#order_num_total_ever_online: The total number of shopping made by the customer on the online platform
-#order_num_total_ever_offline: The total number of shopping made by the customer offline
-#customer_value_total_ever_offline: The total amount paid by the customer for offline shopping
-#customer_value_total_ever_online: The total amount paid by the customer for online shopping
-#customer_value_total_ever_online: The total amount paid by the customer for online shopping
-#interested_in_categories_12: The customer's last List of categories shopped in the last 12 months
-
-
 import pandas as pd
 import numpy as np
 import datetime as dt
